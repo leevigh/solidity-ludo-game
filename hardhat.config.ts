8435171f7bc90c3a7138a8ceda70cdb6b56a85b8
Levi Francis <levifrancis15@gmail.com>
2024-10-04 03:48:35 +0100

    deploy ludo to lisk

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,43 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const ACCOUNT_PRIVATE_KEY = vars.get("SECRET_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    // for testnet
+    "lisk-sepolia": {
+      url: process.env.LISK_RPC_URL!,
+      accounts: [ACCOUNT_PRIVATE_KEY!],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "123",
+    },
+    customChains: [
+      {
+        network: "lisk-sepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
 };
+
+// Deployed Addresses
+
+// LudoModule#Ludo - 0x0bf713601324AAc7841A90Db043a3e3F5e5768D4
 
 export default config;
